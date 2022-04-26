d9952142d73735907d837af68c9cb4306c9fc846
celso14 <celso.botelho14@gmail.com>
2022-04-25 22:34:03 -0300

    Construção primaria do projeto e estilização

---
+++ 5e6b83664f62ed6bde5b5b88/script.js
@@ -0,0 +1,7 @@
+function menuOpener(){
+    if(document.querySelector('#menu-area').style.width == "0px"){
+        document.querySelector('#menu-area').style.width = "200px"
+    }else{
+        document.querySelector('#menu-area').style.width = "0px"
+    }
+}
